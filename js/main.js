083273e9f6518ac01cc41dfd28b58001713b7aaf
munaALboashi <munaelboashi@gmail.com>
2020-08-13 23:13:39 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -1,57 +1,34 @@
-$(document).ready(function () {
-    var json = {
-        'testimonial': [{
-                'img': 'images/image-tanya.jpg',
-                'description': ' Ive been interested in coding for a while <br>but never taken the jump, until now.I <br>couldnt recommend this course enough.<br>Im now in the job of my dreams and so <br> excited about the future.',
-                'signature': 'Tanya Sinclair',
-                'position': 'UX Engineer'
+const items = document.querySelector('.carousel__items');
+const slides = Array.from(items.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
 
-            },
-            {
-                'img': 'images/image-john.jpg',
-                'description': 'If you want to lay the best foundation </br> possible i would recommend taking this </br> course the depth the instructor go into </br> is incredible. I now feel so confident about </br> starting up as a professional developer ',
-                'signature': 'John Tarkpor',
-                'position': 'Junior Front-end Developer'
+const slideWidth = slides[0].getBoundingClientRect().width;
 
-            }
-        ]
-    }
-    var slider = '';
-    $.each(json.testimonial, function (key, val) {
-        slider += ' <div class="first row one' + key + '">'
-        slider += '<div class="testimonial-text col-lg-6 "> <img class = "qoutes" src = "images/pattern-quotes.svg" alt="pattern-quotes">'
-        slider += '<p  class = "text" >' + val.description + '</p>'
-        slider += '<p class = "signature">' + val.signature + ' <span class = "position">' + val.position + ' </span></p> </div>'
-        slider += '<div class="testimonial-pic col-lg-6">  <img class="person-pic" src="' + val.img + ' " alt="person-image"> <img class="pattern-pic" src="images/pattern-bg.svg" alt="pattern-img>'
-        slider += ' <div class="slider-controls"> <button class="prev"><img src="images/icon-prev.svg" alt="next-icon"></button>  <button class="next"><img src="images/icon-next.svg" alt="next-icon"></button> '
-        slider += '</div> </div>  </div>'
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
 
+slides.forEach(setSlidePosition);
 
-    });
-    $('.container').append(slider);
+const moveToSlide = (items, currentSlide, TargetSlide) => {
+    items.style.transform = 'translateX(-' + TargetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    TargetSlide.classList.add('current-slide');
+}
 
-    $('.next').on('click', function () {
-        $('.one1').css({
-            'display': 'block',
-            'animation': 'fade 1.5s'
+prevButton.addEventListener('click', e => {
+    const currentSlide = items.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
 
-        })
-        $('.one0').css({
-            'display': 'none',
+    moveToSlide(items, currentSlide, prevSlide)
+})
+nextButton.addEventListener('click', e => {
+    const currentSlide = items.querySelector('.current-slide');
+
+    const nextSlide = currentSlide.nextElementSibling;
 
 
-        })
-    })
-    $('.prev').on('click', function () {
-        $('.one0').css({
-            'display': 'block',
-            'animation': 'fade 1.5s'
+    moveToSlide(items, currentSlide, nextSlide)
 
-        })
-        $('.one1').css({
-            'display': 'none',
-
-        })
-
-    })
-});
+})
